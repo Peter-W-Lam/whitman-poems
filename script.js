d0dabd5db0f068bd1a873420e24d9c83f3dc244b
Peter Lam <Peter.Lam@Tufts.edu>
2018-12-08 21:05:05 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+$(document).ready(function () {
+
+  var options = {
+    strings: [", then,", "&nbsp;then....", "&nbsp;then"],
+    typeSpeed: 70,
+    backSpeed: 30,
+    loop: true
+  }
+
+
+  var typed = new Typed('#typed-strings', options);
+
+});
